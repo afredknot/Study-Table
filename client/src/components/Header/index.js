322e422461664165c058ac40c96f92547a17e112
Nate <fritz.nathaniel97@gmail.com>
2023-01-17 11:29:42 -0500

    Adds notification handler

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,15 +2,19 @@ import React, { useState } from 'react';
 import ProfileIcon from './ProfileIcon';
 
 const Header = ({iconUrl, onIconClick, title}) => {
+
   const [searchValue, setSearchValue] = useState('');
+
   const handleSearch = (e) => {
     setSearchValue(e.target.value);
   }
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(`Searching for: ${searchValue}`);
     // MAKE QUERY HERE
   }
+  
   return (
     <header>
       <ProfileIcon iconUrl={iconUrl} onClick={onIconClick} />
