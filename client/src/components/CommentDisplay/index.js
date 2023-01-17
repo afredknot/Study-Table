5d256a0efd1c4a9d92ccd0e62464d284bcd77174
Nate <fritz.nathaniel97@gmail.com>
2023-01-17 18:09:52 -0500

    adds CommentDisplay Component

---
+++ client/src/components/CommentDisplay/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const CommentDisplay = ({ comment }) => {
+
+    return (
+        <div>
+            {comment.map((comment) => (
+                <div>
+                    <div>
+                        <p>{comment.author}</p>
+                        <p>{comment.time}</p>
+                        <p>{comment.body}</p>
+                        <button onClick={createReply}>reply</button>
+                    </div>
+
+
+                    {(comment.replies > 0) ? comment.replies.map((reply) => {
+                        <div>
+                            <p>{reply.author}</p>
+                            <p>{reply.time}</p>
+                            <p>{reply.body}</p>
+                        </div>
+                    }) : false}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default CommentDisplay;
