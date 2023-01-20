c7c6794518c6ca2e83388d886403d807d2bc311f
Nate <fritz.nathaniel97@gmail.com>
2023-01-20 15:16:00 -0500

    adds styling to selectors

--- client/src/components/AssignmentSelector/index.js
+++ client/src/components/AssignmentSelector/index.js
@@ -1,33 +1,70 @@
 import React from "react";
 import "./style.css";
 
-import { useProviderContext } from "../../utils/providerContext";
-
 const AssignmentSelector = ({ assignments }) => {
 
-  // const { functions } = useProviderContext();
 
   // ADD STATUS INDICATIOR
   return (
 
-    <div className="card1">
+    <div className="selectors">
       <h3>Assignments</h3>
-      <ul>
-        {/* <li onClick={functions.handleAssignmentSelect}>
-          <h4>Big Fancy Title</h4>
-          <p>Due: 1/22/23</p>
-        </li>
-        <li onClick={functions.handleAssignmentSelect}>
-          <h4>Random Title</h4>
-          <p>Due: 1/24/23</p>
-        </li> */}
-        {/* {assignments.map((assignment) => (
+        <ul>
+          <li>
+            <h4 className="courseTitle">Big Fancy Title</h4>
+            <p className="due">Due: 1/22/23</p>
+          </li>
+          <li>
+            <h4 className="courseTitle">Random Title</h4>
+            <p className="due">Due: 1/24/23</p>
+          </li>
+          <li>
+            <h4 className="courseTitle">Big Fancy Title</h4>
+            <p className="due">Due: 1/22/23</p>
+          </li>
+          <li>
+            <h4 className="courseTitle">Random Title</h4>
+            <p className="due">Due: 1/24/23</p>
+          </li>
+          <li>
+            <h4 className="courseTitle">Big Fancy Title</h4>
+            <p className="due">Due: 1/22/23</p>
+          </li>
+          <li>
+            <h4 className="courseTitle">Random Title</h4>
+            <p className="due">Due: 1/24/23</p>
+          </li>
+          <li>
+            <h4 className="courseTitle">Big Fancy Title</h4>
+            <p className="due">Due: 1/22/23</p>
+          </li>
+          <li>
+            <h4 className="courseTitle">Random Title</h4>
+            <p className="due">Due: 1/24/23</p>
+          </li>
+          <li>
+            <h4 className="courseTitle">Big Fancy Title</h4>
+            <p className="due">Due: 1/22/23</p>
+          </li>
+          <li>
+            <h4 className="courseTitle">Random Title</h4>
+            <p className="due">Due: 1/24/23</p>
+          </li>
+          <li>
+            <h4 className="courseTitle">Big Fancy Title</h4>
+            <p className="due">Due: 1/22/23</p>
+          </li>
+          <li>
+            <h4 className="courseTitle">Random Title</h4>
+            <p className="due">Due: 1/24/23</p>
+          </li>
+          {/* {assignments.map((assignment) => (
               <li key={assignment.assignmentTitle} onClick={handleAssignmentSelect}>
                 <h4>{assignment.assignmentTitle}</h4>
-                <p>{assignment.assignmentDueDate}</p>
+                <p className="due">{assignment.assignmentDueDate}</p>
               </li> */}
-        {/* ))}   */}
-      </ul>
+          {/* ))}   */}
+        </ul>
     </div>
 
   )
